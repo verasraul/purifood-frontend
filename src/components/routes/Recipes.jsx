fcbe3396f6149fa455b6b4e184ae1c574b431a85
Raul Veras <verasraul@gmail.com>
2024-01-27 05:30:53 -0500

    replaced backend heroku link with development link for Recipes component

--- src/components/routes/Recipes.jsx
+++ src/components/routes/Recipes.jsx
@@ -8,7 +8,7 @@ const Recipes = () => {
 
   const fetchData = async () => {
     try {
-      const res = await axios(`https://purifood-backend.herokuapp.com/recipes`);
+      const res = await axios(`http://localhost:8000/recipes`);
       setRecipes(res.data.MyRecipes);
     } catch (err) {
       console.log(err);
